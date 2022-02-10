24c0b45accbb20ec64a5b0b19286fb01973b460d
RayaneB75 <rayane.belkhir@icloud.com>
2022-02-10 09:46:29 +0100

    Changements port

---
+++ client/route&git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install apt-transport-https -y
+
+
+echo "Quelle interface pour le flux"
+read eth
+echo "Ajout multicast route"
+sudo ip route add 224.0.0.0/8 dev $eth
+sudo ip route add 225.0.0.0/8 dev $eth
+
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh vlc -y
+
+gh repo clone RayaneB75/teleSurIP
--- client/route_client_debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Quelle interface pour le flux"
-read eth
-echo "Ajout multicast route"
-sudo ip route add 224.0.0.0/8 dev $eth
-sudo ip route add 225.0.0.0/8 dev $eth
-
-sudo apt update && sudo apt install apache2 vlc -y
